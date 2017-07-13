a694076b597df19630d23b4bae07367be5b4b550
PatreewIgor <patreewigor@yandex.ru>
2017-07-13 12:05:01 +0300

    done

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,31 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	raise TriangleError if a <= 0 || b <= 0 || c <= 0
+	:equilateral if a == b && a == c && b == c
+	:isosceles if a == b && a != c || a == c && a != b
+	:scalene if a != b && a != c && b != c
 end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+
+
+#   s = (a + b + c) / 2.0
+
+#   # the following must be positive to be a valid triangle
+#   ok = (s - a) * (s - b) * (s - c)
+
+#   if a <= 0 || b <= 0 || c <= 0 || ok <= 0 then
+#     raise TriangleError
+#   end
+
+#   if a == b && b == c then
+#     :equilateral
+#   elsif a == b || a == c || b == c then
+#     :isosceles
+#   else
+#     :scalene
+#   end
+# end
